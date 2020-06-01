135913ee3c8dafda1bb87037eebe700330bdc477
Dmitry Ivoninsky <d.ivoninsky@gmail.com>
2020-06-01 21:43:36 +0300

    Added check for negative numbers for six task

--- src/main/java/com/divoninsky/simpleTasks/task6/Main.java
+++ src/main/java/com/divoninsky/simpleTasks/task6/Main.java
@@ -2,35 +2,51 @@ package com.divoninsky.simpleTasks.task6;
 
 public class Main {
     public static void main(String[] args) {
-        int a1 = 1;
+        int a1 = 100;
         int d = 500;
 
-        System.out.println(checkIfSumOfProgressionOverThanInt(a1, d));
-        System.out.println(checkIfSumOfProgressionOverThanLong(a1, d));
+        System.out.println("for int: " + checkIfSumOfProgressionOverThanInt(a1, d));
+        System.out.println("for long: " + checkIfSumOfProgressionOverThanLong(a1, d));
     }
 
     private static int checkIfSumOfProgressionOverThanInt(int a1, int d) {
         int sumOfProgression = 0;
         int count = 1;
         int lastNumber = a1;
-        while (sumOfProgression >= 0){
-            lastNumber+=d;
-            count++;
-            sumOfProgression = (a1+lastNumber)*count/2;
+        if (d > 0){
+            while (sumOfProgression >= 0){
+                lastNumber+=d;
+                count++;
+                sumOfProgression += lastNumber;
+            }
+            return count;
         }
-        return lastNumber;
+        while (sumOfProgression <= 0){
+            sumOfProgression -= lastNumber;
+            lastNumber-=d;
+            count++;
+        }
+        return count;
     }
 
     private static long checkIfSumOfProgressionOverThanLong(int a1, int d) {
         long sumOfProgression = 0;
         long count = 1;
         long lastNumber = a1;
-        while (sumOfProgression >= 0){
-            lastNumber+=d;
-            count++;
-            sumOfProgression = (a1+lastNumber)*count/2;
+        if (d > 0){
+            while (sumOfProgression >= 0){
+                lastNumber+=d;
+                count++;
+                sumOfProgression += lastNumber;
+            }
+            return count;
         }
-        return lastNumber;
+        while (sumOfProgression <= 0){
+            sumOfProgression -= lastNumber;
+            lastNumber-=d;
+            count++;
+        }
+        return count;
     }
 
 
